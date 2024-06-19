f455c01e95867ac992df0da23b01b50db5ff3a61
ernstudy <hey@ernstudy.com>
2024-06-19 11:43:45 -0600

    menu and responsive

--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
-document.querySelector(".menu").addEventListener("click", () => {
+const menu = document.querySelector(".menu");
+const menuIcon = document.querySelector(".bx-menu");
+
+menu.addEventListener("click", () => {
   document.querySelector(".nav").classList.toggle("show-menu");
+  if (menuIcon.classList.contains("bx-menu")) {
+    menuIcon.classList.replace("bx-menu", "bx-x");
+  } else {
+    menuIcon.classList.replace("bx-x", "bx-menu");
+  }
 });
